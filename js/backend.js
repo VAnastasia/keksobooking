335a8aa829288bdd7ab1f2e56cd9bde6e73378c8
Anastasiya <v.anastasiya26@mail.ru>
2019-06-28 19:30:18 +0700

    функция отправки формы

--- js/backend.js
+++ js/backend.js
@@ -13,12 +13,31 @@
       xhr.addEventListener('load', function () {
         if (xhr.status === STATUS_OK) {
           onLoad(xhr.response);
+          window.backend.response = xhr.response;
         } else {
           onError();
         }
       });
 
       xhr.send();
+    },
+
+    save: function (data, onLoad, onError) {
+      var URL = 'https://js.dump.academy/keksobooking';
+      var xhr = new XMLHttpRequest();
+
+      xhr.responseType = 'json';
+
+      xhr.addEventListener('load', function () {
+        if (xhr.status === 200) {
+          onLoad(xhr.response);
+        } else {
+          onError();
+        }
+      });
+
+      xhr.open('POST', URL);
+      xhr.send(data);
     }
   };
 
